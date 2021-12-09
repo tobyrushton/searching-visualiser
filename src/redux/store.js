8a21704d6dfeb3ebe3a907858c15cd22eb854dab
tobyrushton <tobyjrusthon@icloud.com>
2021-12-09 16:20:56 +0000

    Updated reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,10 @@
 import { configureStore} from "@reduxjs/toolkit";
-import arrayReducer from "./reducers/array";
-import searchNum from "./reducers/searchNum";
-import maxValue from "./reducers/maxValue";
-import arrayBars from "./reducers/arrayBars";
-import sortedStatus from "./reducers/sortedStatus";
 import count from "./reducers/count";
+import arrayInfo from "./reducers/arrayInfo";
 
 export default configureStore({
     reducer:{
-        array: arrayReducer,
-        searchNumber: searchNum,
-        arrayBars: arrayBars,
-        maxValue: maxValue,
-        sortedStatus: sortedStatus,
         counter: count,
+        arrayInfo: arrayInfo
     }
 });
