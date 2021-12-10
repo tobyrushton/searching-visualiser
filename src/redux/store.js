aef5692a2d276e196c3e228b05609563d9ff0d1b
tobyrushton <tobyjrusthon@icloud.com>
2021-12-10 13:04:40 +0000

    added slider disable

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore} from "@reduxjs/toolkit";
 import count from "./reducers/count";
 import arrayInfo from "./reducers/arrayInfo";
+import headerInfo from "./reducers/headerInfo";
 
 export default configureStore({
     reducer:{
         counter: count,
-        arrayInfo: arrayInfo
+        arrayInfo: arrayInfo,
+        headerInfo: headerInfo
     }
 });
